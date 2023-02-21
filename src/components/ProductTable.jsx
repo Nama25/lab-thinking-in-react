9d4a97d114bacd4bbf143ab09b6c47b7052c4eff
Nadine Machmeier <info@nadinemachmeier.com>
2023-02-21 19:46:08 +0000

    iteration 1 structure components

---
+++ src/components/ProductTable.jsx
@@ -0,0 +1,8 @@
+import ProductRow from "./ProductRow";
+import {useState} from "react";
+
+function ProductTable() {
+    return
+}
+
+export default ProductTable
---
+++ src/components/ProductsPage.jsx
@@ -0,0 +1,11 @@
+import ProductTable from "./ProductTable";
+import SearchBar from "./SearchBar";
+import {useState} from "react";
+
+
+
+function ProductPage() {
+    return
+}
+
+export default ProductPage
