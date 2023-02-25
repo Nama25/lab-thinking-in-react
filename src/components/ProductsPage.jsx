413725461643ee33188d82a5a30d5d2dd643097d
Nadine Machmeier <info@nadinemachmeier.com>
2023-02-25 16:54:04 +0000

    iteration 5 checkbox is still not working

--- src/components/ProductsPage.jsx
+++ src/components/ProductsPage.jsx
@@ -8,16 +8,23 @@ import dataProducts from "../data.json"
 function ProductPage() {
     const [products, setProducts] = useState(dataProducts)
     const [query, setQuery] = useState('');
+    const [check, setCheck] = useState(false);
 
+    // iteration 4 SEARCH
     const filteredProducts = products.filter(product => {
         return product.name.toLowerCase().includes(query.toLowerCase())
       })
 
-    
+    // iteration 5 CHECKBOX
+
+    const handleChange = () => {
+        setCheck(!check)
+    }
+
     return(
         <div>
             <h1>IronStore</h1>
-            <SearchBar query={query} setQuery={setQuery} />
+            <SearchBar query={query} setQuery={setQuery} handleChange={handleChange}/>
             <ProductTable products={filteredProducts}/>
         </div>
     )
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,7 +1,11 @@
-function SearchBar({query, setQuery}) {
+function SearchBar({query, setQuery, check, handleChange}) {
 
     const searchInput = event => {
         setQuery(event.target.value)
+    }
+
+    const checkActive = event => {
+        handleChange(event.target.value)
     }
 
     return(
@@ -9,7 +13,7 @@ function SearchBar({query, setQuery}) {
             <input type="search" value={query} onChange={searchInput}/>
             <label>Search</label>
             <form>
-                <input type="checkbox"/>
+                <input type="checkbox" value={check} onChange={checkActive}/>
                 <label>Only show products in stock</label>
             </form>
         </div>
